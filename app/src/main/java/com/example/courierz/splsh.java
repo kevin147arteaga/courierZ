f8b138471041af0ea2bc13e6ab7383472e471364
kevinarteaga19 <2018160037@udh.edu.pe>
2021-11-11 20:44:29 -0500

    v5

--- app/src/main/java/com/example/courierz/splsh.java
+++ app/src/main/java/com/example/courierz/splsh.java
@@ -1,4 +1,23 @@
 package com.example.courierz;
 
-public class splsh  {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splsh  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splsh);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splsh.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+    }
 }
